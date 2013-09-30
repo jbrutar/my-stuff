3fa5fd8d2f32097bbc960abe1b2215a066978594
Jorgen Brutar <jorgen.brutar@gmail.com>
2013-09-30 21:08:43 +0200

    Commit on master

--- TestCode/src/main/java/se/jorgen/controller/UserController.java
+++ TestCode/src/main/java/se/jorgen/controller/UserController.java
@@ -15,24 +15,25 @@ import se.jorgen.service.UserService;
 @RequestMapping(value = "/user")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+	@Autowired
+	private UserService userService;
 
-    @RequestMapping(value = "/{userId}/get", method = RequestMethod.GET)
-    public ModelAndView get(@PathVariable int userId, Model model) {
-	ModelAndView mav = new ModelAndView("user");
-	User user = userService.findById(userId);
-	mav.addObject("user", user);
+	@RequestMapping(value = "/{userId}/get", method = RequestMethod.GET)
+	public ModelAndView get(@PathVariable int userId, Model model) {
+		ModelAndView mav = new ModelAndView("user");
+		User user = userService.findById(userId);
+		mav.addObject("user", user);
+		int i = 0;
 
-	return mav;
-    }
+		return mav;
+	}
 
-    @RequestMapping(value = "/{userId}/update", method = RequestMethod.POST)
-    public ModelAndView update(@PathVariable int userId, @Valid User user) {
-	ModelAndView mav = new ModelAndView("user");
-	userService.update(user);
-	mav.addObject("user", user);
+	@RequestMapping(value = "/{userId}/update", method = RequestMethod.POST)
+	public ModelAndView update(@PathVariable int userId, @Valid User user) {
+		ModelAndView mav = new ModelAndView("user");
+		userService.update(user);
+		mav.addObject("user", user);
 
-	return mav;
-    }
+		return mav;
+	}
 }
